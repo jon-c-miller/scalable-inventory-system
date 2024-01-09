431c13c60acd887a686772b2586b72cd072ff6f2
Jon Miller <millerj.2203@protonmail.com>
2024-01-09 14:50:31 +0900

    add inventory item stat scriptable object

---
+++ Assets/Game/Elements/InventoryItem/SInventoryItemStat.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Inventory Item Stat", menuName = "Inventory Item/Create New Item Stat")]
+public class SInventoryItemStat : ScriptableObject
+{
+    public string Name;
+    public string Description;
+    public ItemStatIDs Type;
+    public int Value;
+}
+
+public enum ItemStatIDs
+{
+    Size,
+    Quality,
+    EffectRange,
+
+    ManaIncrease,
+    DamageIncrease,
+
+    HealingAmount,
+    ComfortAmount,
+}

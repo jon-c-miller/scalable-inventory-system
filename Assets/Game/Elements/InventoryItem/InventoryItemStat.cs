7e328807121ad729a74107d47b5d0eee0ffb6027
Jon Miller <millerj.2203@protonmail.com>
2024-01-09 15:47:52 +0900

    add a setter to item stat's value property

--- Assets/Game/Elements/InventoryItem/InventoryItemStat.cs
+++ Assets/Game/Elements/InventoryItem/InventoryItemStat.cs
@@ -7,5 +7,5 @@ public class InventoryItemStat : MonoBehaviour
     public string Name => properties.Name;
     public string Description => properties.Description;
     public ItemStatIDs Type => properties.Type;
-    public int Value => properties.Value;
+    public int Value { get => properties.Value; set => properties.Value = value; }
 }

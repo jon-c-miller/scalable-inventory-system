efeb9b36c8de121c41fb8ac3753e50b2ab83fada
Jon Miller <millerj.2203@protonmail.com>
2024-01-11 16:21:08 +0900

    integrate updated inventory system api with canvas tester

--- Assets/Game/Elements/Testing/CanvasItemReader.cs
+++ Assets/Game/Elements/Testing/CanvasItemReader.cs
@@ -4,28 +4,51 @@ public class CanvasItemReader : MonoBehaviour
 {
     [SerializeField] KeyCode updateUIValuesFromItem = KeyCode.Space;
     [Space]
-    [SerializeField] SInventoryItem item;
-    [Space]
+    [SerializeField] InventoryItem instantiatedItem;
+
+    [Header("Generation Test Parameters")]
+    [SerializeField] ItemIDs desiredItemType;
+    [SerializeField] ItemQualityIDs desiredItemQuality;
+    [SerializeField, Range(1, 20)] int desiredItemLevel;
+    
+    [Header("UI Fields")]
     [SerializeField] UnityEngine.UI.Text nameText;
     [SerializeField] UnityEngine.UI.Text descriptionText;
+    [SerializeField] UnityEngine.UI.Text quantityText;
     [SerializeField] UnityEngine.UI.Text[] propertyTexts;
 
-    void UpdateTextBasedOnScriptableObject()
+    public void UpdateTextBasedOnItem(InventoryItem newItem)
     {
-        nameText.text = item.Name;
-        descriptionText.text = item.Description;
+        instantiatedItem = newItem;
 
-        for (int i = 0; i < item.Stats.Length; i++)
+        nameText.text = instantiatedItem.ItemName;
+        descriptionText.text = instantiatedItem.ItemDescription;
+
+        for (int i = 0; i < instantiatedItem.ItemStats.Length; i++)
         {
-            propertyTexts[i].text = item.Stats[i].Name;
+            // Handle the quantity stat differently from other stats
+            if (instantiatedItem.ItemStats[i].Type == ItemStatIDs.Quantity)
+            {
+                quantityText.text = instantiatedItem.ItemStats[i].Value.ToString();
+            }
+            else
+            {
+                propertyTexts[i].text = FormatBasedOnStatType(instantiatedItem.ItemStats[i]);
+            }
         }
+    }
+
+    string FormatBasedOnStatType(InventoryItemStat stat)
+    {
+        return "";
     }
 
     void Update()
     {
         if (Input.GetKeyDown(updateUIValuesFromItem))
         {
-            UpdateTextBasedOnScriptableObject();
+            InventoryItem itemBasedOnDesiredType = InventoryItemGenerator.CreateInventoryItem(desiredItemType, desiredItemQuality, desiredItemLevel);
+            UpdateTextBasedOnItem(itemBasedOnDesiredType);
         }
     }
 }

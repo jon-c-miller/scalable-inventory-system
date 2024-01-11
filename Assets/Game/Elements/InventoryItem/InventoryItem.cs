19b8003942e278ee780a8f7721d6eed7cf344b44
Jon Miller <millerj.2203@protonmail.com>
2024-01-11 14:28:40 +0900

    update inventory item to be a struct that holds fields assignable based on a scriptable template

--- Assets/Game/Elements/InventoryItem/InventoryItem.cs
+++ Assets/Game/Elements/InventoryItem/InventoryItem.cs
@@ -1,11 +1,23 @@
 using UnityEngine;
 
-public class InventoryItem : MonoBehaviour
+[System.Serializable]
+public struct InventoryItem
 {
-    [SerializeField] SInventoryItem properties;
+    [SerializeField] string itemName;
+    [SerializeField] string itemDescription;
+    [SerializeField] ItemIDs itemType;
+    [SerializeField] InventoryItemStat[] itemStats;
 
-    public string Name => properties.Name;
-    public string Description => properties.Description;
-    public ItemIDs Type => properties.Type;
-    public SInventoryItemStat[] Stats => properties.Stats;
+    public readonly string ItemName => itemName;
+    public readonly string ItemDescription => itemDescription;
+    public readonly ItemIDs ItemType => itemType;
+    public readonly InventoryItemStat[] ItemStats => itemStats;
+
+    public InventoryItem(string name, string description, ItemIDs type, InventoryItemStat[] stats)
+    {
+        itemName = name;
+        itemDescription = description;
+        itemType = type;
+        itemStats = stats;
+    }
 }

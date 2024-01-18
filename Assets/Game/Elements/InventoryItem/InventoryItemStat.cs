78607d1f6773c9c54464685b4b387608a772e25b
Jon Miller <millerj.2203@protonmail.com>
2024-01-18 14:16:48 +0900

    remove name/description fields from item stats (can be retrieved frorm item stat database)

--- Assets/Game/Elements/InventoryItem/InventoryItemStat.cs
+++ Assets/Game/Elements/InventoryItem/InventoryItemStat.cs
@@ -1,20 +1,14 @@
 [System.Serializable]
 public struct InventoryItemStat
 {
-    [UnityEngine.SerializeField] string statName;
-    [UnityEngine.SerializeField] string statDescription;
     [UnityEngine.SerializeField] ItemStatTypes statType;
     [UnityEngine.SerializeField] int statValue;
 
-    public readonly string Name => statName;
-    public readonly string Description => statDescription;
     public readonly ItemStatTypes Type => statType;
     public readonly int Value => statValue;
 
-    public InventoryItemStat(string name, string description, ItemStatTypes type, int value)
+    public InventoryItemStat(ItemStatTypes type, int value)
     {
-        statName = name;
-        statDescription = description;
         statType = type;
         statValue = value;
     }

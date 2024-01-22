c823b0b65f1a899500d6811658356000f7ae21ec
Jon Miller <millerj.2203@protonmail.com>
2024-01-22 17:46:07 +0900

    move inventory removing logic into a separate class

--- Assets/Game/Elements/Inventory/Inventory.cs
+++ Assets/Game/Elements/Inventory/Inventory.cs
@@ -20,42 +20,7 @@ public class Inventory
 
     public void RemoveItemByType(ItemTypes itemTypeToRemove)
     {
-        // Loop backwards to remove the entry farthest from the start (helps support a compact inventory)
-        int inventorySize = currentInventory.Count - 1;
-        UnityEngine.Debug.LogWarning($"Loop backwards through the entire inventory ({currentInventory.Count} items) to check for item...");
-        for (int i = inventorySize; i >= 0; i--)
-        {
-            // Filter for items of the same type
-            UnityEngine.Debug.LogWarning($"Filter for items of the same type...");
-            if (currentInventory[i].ItemType == itemTypeToRemove)
-            {
-                // Handle cases where the item to be removed is stackable
-                if (InventoryItemDatabase.ItemDatabase[itemTypeToRemove].IsStackable)
-                {
-                    int currentQuantity = currentInventory[i].ItemQuantity;
-
-                    // Remove entry if there is only 1 quantity left
-                    if (currentQuantity == 1)
-                    {
-                        UnityEngine.Debug.LogWarning($"Eliminate the entry due to remaining quantity of 1...");
-                        currentInventory[i] = new();
-                        break;
-                    }
-
-                    // Reduce the current stack and update the entry
-                    UnityEngine.Debug.LogWarning($"Reduce the current stack and update the entry...");
-                    InventoryItem inventoryUpdate = currentInventory[i].CopyItem(currentQuantity - 1);
-                    currentInventory[i] = inventoryUpdate;
-                    break;
-                }
-                else
-                {
-                    UnityEngine.Debug.LogWarning($"Eliminate the entry...");
-                    currentInventory[i] = new();
-                    break;
-                }
-            }
-        }
+        InventoryRemove.RemoveItemByType(currentInventory, itemTypeToRemove);
     }
 
     public void RemoveItemByID(int itemID)

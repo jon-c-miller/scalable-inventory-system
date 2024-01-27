8a6d21d7c2c6c249079a45efc25a177154600e8c
Jon Miller <millerj.2203@protonmail.com>
2024-01-27 09:52:44 +0900

    add the option to hide quantity on the item viewer

--- Assets/Game/Elements/InventoryItem/View/ItemViewer.cs
+++ Assets/Game/Elements/InventoryItem/View/ItemViewer.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 [System.Serializable]
 public class ItemViewer : MonoBehaviour, IItemView
 {
+    [SerializeField] bool showQuantity;
     [Header("UI Fields")]
     [SerializeField] UnityEngine.UI.Text nameText;
     [SerializeField] UnityEngine.UI.Text descriptionText;
@@ -27,8 +28,8 @@ public class ItemViewer : MonoBehaviour, IItemView
         // Keep track of the property text index to allow flexibility based on property type
         int propertyTextIndex = 0;
 
-        // Display item quantity if over 1
-        quantityText.text = item.ItemQuantity > 1 ? item.ItemQuantity.ToString() : "";
+        // Display item quantity if over 1 and showQuantity is true
+        quantityText.text = item.ItemQuantity > 1 && showQuantity ? item.ItemQuantity.ToString() : "";
 
         qualityText.text = StatTextFormatter.FormatQualityText(item.ItemQuality);
 

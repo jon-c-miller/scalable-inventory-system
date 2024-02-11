578494d44d19cbe9840d7cfb4579bc84de07603b
Jon Miller <millerj.2203@protonmail.com>
2024-02-12 07:16:45 +0900

    add inventory database accessors to game api

--- Assets/Game/Elements/GameAPI/InventoryAPI.cs
+++ Assets/Game/Elements/GameAPI/InventoryAPI.cs
@@ -5,7 +5,23 @@ public partial class GameAPI
 {
     [SerializeField] InventoryManager inventoryManager = new();
 
+    // Database API
+
+    public string InventoryGetItemName(ItemIDs id) => inventoryManager.GetItemName(id);
+
+    public string InventoryGetItemStatName(ItemStatIDs id) => inventoryManager.GetStatName(id);
+
+    public string InventoryGetItemDescription(ItemIDs id) => inventoryManager.GetItemDescription(id);
+
+    public string InventoryGetStatDescription(ItemStatIDs id) => inventoryManager.GetStatDescription(id);
+
+    public SInventoryItem InventoryGetItemTemplate(ItemIDs id) => inventoryManager.GetItemTemplate(id);
+
+    public SInventoryItemStat InventoryGetItemStatTemplate(ItemStatIDs id) => inventoryManager.GetItemStatTemplate(id);
+
+
     // Inventory API
+
     public InventoryItem InventoryGenerateSpecificItem(ItemIDs type, ItemQualityIDs quality, int level) => InventoryItemGenerator.CreateSpecificItem(type, quality, level);
 
     public InventoryItem InventoryGenerateItemAvailableAtLevel(int level) => InventoryItemGenerator.CreateRandomItemAvailableAtLevel(level);
